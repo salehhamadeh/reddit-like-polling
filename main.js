07a72dbe3407caf864ac05e45d6b00de933b2850
Saleh Hamadeh <salehhamadeh@gmail.com>
2015-03-16 14:02:37 -0400

    Add userid so that a person cannot vote for the same topic more than once.

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var topics = [];
+var nUsers = 0;
 
 app.use(express.static('public'));
 
@@ -16,21 +17,56 @@ io.on('connection', function(socket) {
 
 	socket.on('new topic', function(data) {
 		data.id = topics.length;
-		data.votes = 0;
+		data.votes = [];
 		topics.push(data);
+
+		// Only send the number of votes to client
+		data = clone(data);
+		data.votes = data.votes.length;
+
 		io.emit('new topic', data);
 		console.log('new topic added!');
 	});
 
 	socket.on('request topics', function() {
-		socket.emit('request topics', topics);
+		socket.emit('request topics', topics.map(function(topic) {
+			var ret = clone(topic);
+			ret.votes = topic.votes.length;
+			return ret;
+		}));
 	});
 
-	socket.on('vote', function(topicId) {
-		topics[topicId].votes++;
-		io.emit('update topic', topics[topicId]);
+	socket.on('request userid', function() {
+		socket.emit('request userid', nUsers++);
+	})
+
+	socket.on('toggle vote', function(data) {
+		// If the user had already voted, remove vote. Otherwise, add vote.
+		var topic = clone(topics[data.topicId]);
+		if (data.userid in topic.votes) {
+			console.log("YES");
+			var index = topic.votes.indexOf(data.userid);
+			topics[data.topicId].votes.splice(index, 1);
+		} else {
+			console.log("NO");
+			topics[data.topicId].votes.push(data.userid);
+		}
+
+		// Only send the number of votes to client
+		topic.votes = topics[data.topicId].votes.length;
+
+		io.emit('update topic', topic);
 	});
 });
+
+function clone(obj) {
+    if (null == obj || "object" != typeof obj) return obj;
+    var copy = obj.constructor();
+    for (var attr in obj) {
+        if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+    }
+    return copy;
+}
 
 http.listen(3000, function() {
 	console.log('listening on *:3000');
